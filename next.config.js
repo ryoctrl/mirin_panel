eec998fe8ddca765f6164047f2ba11d49a6de90c
ryoctrl <ryoctrl@gmail.com>
2020-10-26 02:45:59 +0900

    正直 of the 三度目

--- next.config.js
+++ next.config.js
@@ -16,6 +16,7 @@ const nextConfig = {
     appId: process.env.appId,
     measurementId: process.env.measurementId,
     PANEL_BACKEND_ADDRESS: process.env.PANEL_BACKEND_ADDRESS,
+    PANEL_FRONTEND_ADDRESS: process.env.PANEL_FRONTEND_ADDRESS,
   },
 };
 
